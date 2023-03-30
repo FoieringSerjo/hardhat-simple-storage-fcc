2dfa7fe82d9a745ce529cc6abcd84fa8b882d30c
Foiering Sergey <serjofoi@gmail.com>
2023-03-30 11:42:19 +0300

    hardhat contract example

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,26 +1,26 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("./tasks/block-number")
-require("@nomiclabs/hardhat-etherscan")
-require("dotenv").config()
-require("solidity-coverage")
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
+require('./tasks/block-number');
+require('@nomiclabs/hardhat-etherscan');
+require('solidity-coverage');
+require('dotenv').config();
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || '';
 const SEPOLIA_RPC_URL =
   process.env.SEPOLIA_RPC_URL ||
-  "https://eth-sepolia.g.alchemy.com/v2/your-api-key"
+  'https://eth-sepolia.g.alchemy.com/v2/your-api-key';
 const PRIVATE_KEY =
   process.env.PRIVATE_KEY ||
-  "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
 
 module.exports = {
-  defaultNetwork: "hardhat",
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
     sepolia: {
@@ -29,19 +29,19 @@ module.exports = {
       chainId: 11155111,
     },
     localhost: {
-      url: "http://localhost:8545",
+      url: 'http://localhost:8545',
       chainId: 31337,
     },
   },
-  solidity: "0.8.8",
+  solidity: '0.8.8',
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
   gasReporter: {
-    enabled: true,
-    currency: "USD",
-    outputFile: "gas-report.txt",
+    enabled: false,
+    currency: 'USD',
+    outputFile: 'gas-report.json',
     noColors: true,
-    coinmarketcap: COINMARKETCAP_API_KEY,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
   },
-}
+};
